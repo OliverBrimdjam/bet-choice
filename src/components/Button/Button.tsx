c80226fc24f4d0bba28f772281aacc566b8bbc38
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2024-10-22 23:10:36 -0300

    [feat] add button Icon type check;

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,23 @@
+import Image from "next/image";
 
 type TButtonProps = {
-    icon?: React.ReactNode,
+    icon?: React.ReactNode | string,
     children?: React.ReactNode,
 }
 
 export default function Button({ icon, children }: TButtonProps) {
     return (
         <button className="bg-blue-900 text-white px-4 py-3 min-w-44 rounded-md flex flex-row justify-center shadow-sm hover:shadow-xl">
-            {icon}
+            {typeof icon === 'string'
+                ? <Image
+                    src={icon}
+                    alt="Icon"
+                    width={24}
+                    height={24}
+                    className="mx-2"
+                />
+                : <span className="mx-2">{icon}</span>
+            }
             {children}
         </button>
     );
