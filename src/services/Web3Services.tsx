820a04f9a32d96578d8eab97706c6d4e11548517
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2024-10-24 08:09:45 -0300

    [feat] implement signIn function;

---
+++ src/services/Web3Services.tsx
@@ -0,0 +1,13 @@
+import Web3 from 'web3';
+
+export async function singIn() {
+
+    if (!window.ethereum) throw new Error('No wallet detected.');
+
+    const web3 = new Web3(window.ethereum);
+    const signedIn = await web3.eth.requestAccounts();
+    if (!signedIn || !signedIn.length) throw new Error('Unauthorized wallet.');
+
+    localStorage.setItem("MetaMaskAccount", signedIn[0]);
+    return signedIn[0];
+}
